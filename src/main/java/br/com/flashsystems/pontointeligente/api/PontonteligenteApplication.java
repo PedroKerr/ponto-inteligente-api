5709fe4d49d701a1186f136a02ccd334a6f422f6
unknown <pedro.kerr@flashsystems.com.br>
2020-06-22 21:50:41 -0300

    Commit Inicial dos Arquivos do Projeto

---
+++ src/main/java/br/com/flashsystems/pontointeligente/api/PontonteligenteApplication.java
@@ -0,0 +1,13 @@
+package br.com.flashsystems.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontonteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontonteligenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/flashsystems/pontointeligente/api/PontonteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.flashsystems.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontonteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
